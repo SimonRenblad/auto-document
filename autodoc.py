3babde6a3c14f3f48c64e1089cb4b58d30d4f6d3
Simon Renblad <simon.renblad@outlook.com>
2021-01-04 10:51:19 +0800

    ignore dot, formatting and rel paths

--- autodoc.py
+++ autodoc.py
@@ -5,28 +5,33 @@ import getopt
 recursive_depth = 3
 print_dot = False
 
-def print_folder(directory, current_depth):
+def print_folder(working_dir, relative_dir, current_depth):
     # recursive function to print the contents of the folders
 
     if current_depth == recursive_depth:
-        print("depth reached")
         return
+
+    directory = os.path.join(working_dir, relative_dir)
+
+    print("__________________________")
+    print()
+    print(relative_dir)
+    print("__________________________")
 
     for f in os.listdir(directory):
         print(f)
 
-    print("__________________________")
-
     for f in os.listdir(directory):
         d = os.path.join(directory, f)
+        rd = os.path.join(relative_dir, f)
         if os.path.isdir(d):
             if(f[0] != '.' or print_dot):
-                print_folder(d, current_depth + 1)
+                print_folder(working_dir, rd, current_depth + 1)
 
 def main(argv):
     # get cli arguments
     try:
-        opts, args = getopt.getopt(argv, "d:")
+        opts, args = getopt.getopt(argv, "d:p")
     except getopt.GetoptError:
         print("invalid option")
         sys.exit(2)
@@ -34,8 +39,10 @@ def main(argv):
     for opt, arg in opts:
         if opt == '-d':
             recursive_depth = int(arg)
+        elif opt == '-p':
+            print_dot = True
 
-    print_folder(os.getcwd(), 0)
+    print_folder(os.getcwd(), '.', 0)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
