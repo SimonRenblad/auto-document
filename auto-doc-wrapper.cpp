841a9ab2612ce66039bde8b2859111216317ef37
Simon Renblad <simon.renblad@outlook.com>
2021-01-06 00:08:28 +0800

    finished wrapper
    added hard-coded path
    fixed depth option
    fixed wrapper
    allow non-ascii (kinda, still an issue)

--- auto-doc-wrapper.cpp
+++ auto-doc-wrapper.cpp
@@ -6,12 +6,12 @@ int main(int argc, char *argv[]) {
    std::vector<std::string> args;
    for(int i = 1; i < argc; i++)
        args.push_back(std::string(argv[i]));
-   std::string command = "python autodoc.py";
+   std::string command = "python C:\\Users\\simon\\Documents\\GitHub\\auto-document\\autodoc.py";
    for(auto it = args.begin(); it != args.end(); it++) {
        command.append(std::string(" "));
        command.append(*it);
    }
    command.append(std::string(" > result.html"));
-   int i = std::system(command);
+   int i = std::system(command.c_str());
    return i;
 }
