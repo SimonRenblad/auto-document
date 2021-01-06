8d236d1a2d559f47258f3bbc9a54bab6d85fcd6b
Simon Renblad <simon.renblad@outlook.com>
2021-01-06 11:34:30 +0800

    adding .autodoc tmp folder
    - add pathenv file for wrapper use
    - add autodoc install {path} command
    - fix print_dot
    - move file creation to python to support unicode

--- auto-doc-wrapper.cpp
+++ auto-doc-wrapper.cpp
@@ -1,17 +1,59 @@
 #include <cstdlib>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <iostream>
+
+void save_path(std::string path) {
+	// attempt to make .autodoc directory
+	std::mkdir(".autodoc");
+	
+	// create file
+	std::ofstream path_file;
+	path_file.open(".autodoc\\pathenv", ios::out | ios::trunc);
+	path_file << path;
+	path_file.close();
+	return;
+}
+
+std::string get_path() {
+	try {
+		std::ofstream path_file;
+		path_file.open(".autodoc\\pathenv", ios::in);
+		if(!path_file.is_open())
+			throw "failed to find pathenv, have you run autodoc install {path}?";
+		std::string path;
+		std::getline(path_file, path); // get the first line
+		return path;
+	} catch(std::string msg) {
+		std::cout << msg << std::endl;
+		std::exit(2); // exit with error status
+	}
+	return ""; // to satisfy compiler
+}
 
 int main(int argc, char *argv[]) {
    std::vector<std::string> args;
    for(int i = 1; i < argc; i++)
        args.push_back(std::string(argv[i]));
-   std::string command = "python C:\\Users\\simon\\Documents\\GitHub\\auto-document\\autodoc.py";
+   
+   // install prior to use
+   if(args[0] == "install") {
+	   std::string path = args[1];
+	   save_path(path);
+	   return;
+   }
+   
+   std::string path = get_path(); // get the path to python file
+   
+   std::string command = "python ";
+   command.append(path.rstrip('\\'));
+   command.append("\\autodoc.py");
+   
    for(auto it = args.begin(); it != args.end(); it++) {
        command.append(std::string(" "));
        command.append(*it);
    }
-   command.append(std::string(" > result.html"));
    int i = std::system(command.c_str());
    return i;
 }
--- autodoc.py
+++ autodoc.py
@@ -2,15 +2,40 @@ import os
 import sys
 import getopt
 
-print_dot = False
+def check_tmp():
+    # check for a .autodoc directory
+    ad = os.path.join(os.getcwd(), ".autodoc")
+    try:
+        os.mkdir(ad)
+    except FileExistsError:
+        pass
+        
+    return ad
 
-def print_folder(working_dir, relative_dir, previous_relative_dir, current_depth, recursive_depth):
+def prepare_file(tmp_dir):
+    # refresh the file to put documentation
+    name = str(os.path.join(tmp_dir, "index.html"))
+    f = open(name, "w")
+    f.close()
+    return
+    
+def add_folder_to_file(folder):
+    # append to file
+    name = os.path.join(os.getcwd(), ".autodoc\\index.html")
+    with open(name, "a", encoding="utf-8") as f:
+        f.write(folder)
+        f.close()
+    return
+
+def print_folder(relative_dir, previous_relative_dir, current_depth, recursive_depth, print_dot):
     # recursive function to print the contents of the folders
+    
+    folder = ""
 
     if current_depth == recursive_depth:
         return
 
-    directory = os.path.join(working_dir, relative_dir)
+    directory = os.path.join(os.getcwd(), relative_dir)
 
     # determine if there shld be a back
     if previous_relative_dir != 0:
@@ -19,31 +44,32 @@ def print_folder(working_dir, relative_dir, previous_relative_dir, current_depth
         back_link = u""
 
     # complete link
-    title_complete = u"<a name=\"" + str(relative_dir) + u"\">" + str(relative_dir) + u"</a>" + back_link + u"</br>"
+    title_complete = u"<a name=\"" + str(relative_dir) + u"\">" + str(relative_dir) + u"</a>" + back_link + u"</br>\n"
 
-    print("__________________________</br>".encode("utf-8"))
-    print("</br>".encode("utf-8"))
-    print(title_complete.encode("utf-8"))
-    print("__________________________</br>".encode("utf-8"))
-    print("</br>".encode("utf-8"))
+    folder += "__________________________</br>\n"
+    folder += "</br>\n"
+    folder += title_complete
+    folder += "__________________________</br>\n"
+    folder += "</br>\n"
 
     for f in os.listdir(directory):
         d = os.path.join(directory, f)
         rd = os.path.join(relative_dir, f)
         if os.path.isdir(d):
             if(f[0] != '.' or print_dot):
-                s = u"<a href=\"#" + str(rd) + u"\">" + str(f) + u"</a></br>"
-                print(s.encode("utf-8"))
+                folder += u"<a href=\"#" + str(rd) + u"\">" + str(f) + u"</a></br>\n"
         else:
-            s = str(f) + u"</br>"
-            print(s.encode("utf-8"))
+            folder += str(f) + u"</br>\n"
+            
+    # save to file
+    add_folder_to_file(folder)
 
     for f in os.listdir(directory):
         d = os.path.join(directory, f)
         rd = os.path.join(relative_dir, f)
         if os.path.isdir(d):
             if(f[0] != '.' or print_dot):
-                print_folder(working_dir, rd, relative_dir, current_depth + 1, recursive_depth)
+                print_folder(rd, relative_dir, current_depth + 1, recursive_depth, print_dot)
 
 def main(argv):
     # get cli arguments
@@ -53,13 +79,17 @@ def main(argv):
         print("invalid option")
         sys.exit(2)
     recursive_depth = 3
+    print_dot = False
     for opt, arg in opts:
         if opt == '-d':
             recursive_depth = int(arg)
         elif opt == '-p':
             print_dot = True
+            
+    tmp_dir = check_tmp()
+    prepare_file(tmp_dir)
 
-    print_folder(os.getcwd(), '.', 0, 0, recursive_depth)
+    print_folder('.', 0, 0, recursive_depth, print_dot)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
